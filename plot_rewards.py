be1e7338f462d42e5753e1dbe161097cf3559ec1
mdecke <mdecke@student.ethz.ch>
2026-04-29 12:12:13 -0700

    add color bar and learning starts indicator

--- plot_rewards.py
+++ plot_rewards.py
@@ -1,5 +1,7 @@
 import os
+import json
 import argparse
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -20,14 +22,20 @@ if not run_dirs:
     exit(1)
 
 plt.figure(figsize=(10, 5))
+cmap = plt.get_cmap('coolwarm')
+colors = cmap(np.linspace(0,1,len(run_dirs)))
 
-for run in run_dirs:
+for k, run in enumerate(run_dirs):
     df = pd.read_csv(os.path.join(logs_dir, run, "performance.csv"))
     returns = df["mean_return"]
     if args.smooth > 1:
         returns = returns.rolling(args.smooth, min_periods=1).mean()
-    plt.plot(df["timestep"], returns, label=run)
-
+    plt.plot(df["timestep"], returns, label=run, color=colors[k])
+    h_params_path = os.path.join(logs_dir, run, 'hyperparams.json')
+    with open(h_params_path,'r') as file:
+        params_dict = json.load(file)
+    plt.vlines(params_dict['training']['learning_starts'], -1500, 0, linestyles='--', label=f'learning starts - {run}', colors=colors[k])
+plt.hlines(0, xmin=0, xmax=params_dict['training']['max_interactions'], colors='k', linewidth=1.2)
 plt.xlabel("Environment interactions")
 plt.ylabel("Mean return")
 plt.title("Training return")
